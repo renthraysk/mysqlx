c080f587259c6998982e30bdf9a917253ac38c57
renthraysk <renthraysk@gmail.com>
2018-04-12 17:09:53 +0100

    Removed database/sql testing decorator. Added query() helper.

--- benchmark_test.go
+++ benchmark_test.go
@@ -5,37 +5,6 @@ import (
 	"testing"
 )
 
-func BenchmarkSimpleExec(b *testing.B) {
-	b.ReportAllocs()
-	db := NewDB(b)
-	defer db.Close()
-	for i := 0; i < b.N; i++ {
-		if _, err := db.ExecContext(context.Background(), "DO 1"); err != nil {
-			b.Fatal(err)
-		}
-	}
-}
-
-func BenchmarkPreparedExec(b *testing.B) {
-	b.ReportAllocs()
-	db := NewDBFatalErrors(b)
-	defer db.Close()
-	stmt, _ := db.PrepareContext(context.Background(), "DO 1")
-	for i := 0; i < b.N; i++ {
-		stmt.Exec()
-	}
-}
-
-func BenchmarkSimpleQueryRow(b *testing.B) {
-	b.ReportAllocs()
-	db := NewDBFatalErrors(b)
-	defer db.Close()
-	var num int
-	for i := 0; i < b.N; i++ {
-		db.QueryRowContext(context.Background(), "SELECT 1").Scan(&num)
-	}
-}
-
 func BenchmarkConnectAuthentication(b *testing.B) {
 	b.ReportAllocs()
 	connector := NewConnector(b)
--- helper_test.go
+++ helper_test.go
@@ -17,24 +17,12 @@ var tlsConfig = &tls.Config{InsecureSkipVerify: true}
 func NewConnector(tb testing.TB) *Connector {
 	tb.Helper()
 
-	connector, err := New("tcp", ipAddress, WithUserPassword("usernative", "passwordnative"), WithDatabase("sakila"))
+	connector, err := New("tcp", ipAddress,
+		WithUserPassword("usernative", "passwordnative"))
 	if err != nil {
 		tb.Fatalf("creating connector failed: %s", err)
 	}
 	return connector
-}
-
-type DB interface {
-	ExecContext(ctx context.Context, stmt string, args ...interface{}) (sql.Result, error)
-
-	QueryContext(ctx context.Context, stmt string, args ...interface{}) (*sql.Rows, error)
-	QueryRowContext(ctx context.Context, stmt string, args ...interface{}) *sql.Row
-
-	PrepareContext(ctx context.Context, stmt string) (*sql.Stmt, error)
-
-	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
-
-	Close() error
 }
 
 func NewDB(tb testing.TB) *sql.DB {
@@ -42,57 +30,29 @@ func NewDB(tb testing.TB) *sql.DB {
 	return sql.OpenDB(NewConnector(tb))
 }
 
-func NewDBFatalErrors(tb testing.TB) DB {
+func query(tb testing.TB, sql string, args []interface{}, scan func(rows *sql.Rows) error) {
 	tb.Helper()
-	return &DBFatal{NewDB(tb), tb}
-}
 
-type DBFatal struct {
-	*sql.DB
-	tb testing.TB
-}
-
-func (d *DBFatal) ExecContext(ctx context.Context, stmt string, args ...interface{}) (sql.Result, error) {
-	d.tb.Helper()
-	r, err := d.DB.ExecContext(ctx, stmt, args...)
+	db := NewDB(tb)
+	defer db.Close()
+	rows, err := db.QueryContext(context.Background(), sql, args...)
 	if err != nil {
-		d.tb.Fatalf("Exec failed: %s", err)
+		tb.Fatalf("QueryContext failed: %s", err)
 	}
-	return r, err
-}
-
-func (d *DBFatal) QueryContext(ctx context.Context, stmt string, args ...interface{}) (*sql.Rows, error) {
-	d.tb.Helper()
-	r, err := d.DB.QueryContext(ctx, stmt, args...)
-	if err != nil {
-		d.tb.Fatalf("Query failed: %s", err)
+	defer rows.Close()
+	if !rows.Next() {
+		tb.Fatalf("no row returned: %s", rows.Err())
 	}
-	return r, err
-}
-
-func (d *DBFatal) Prepare(stmt string) (*sql.Stmt, error) {
-	d.tb.Helper()
-	s, err := d.DB.Prepare(stmt)
-	if err != nil {
-		d.tb.Fatalf("Prepare failed: %s", err)
+	if err := scan(rows); err != nil {
+		tb.Fatalf("scan failed: %s", err)
 	}
-	return s, err
-}
-
-func (d *DBFatal) BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) {
-	d.tb.Helper()
-	s, err := d.DB.BeginTx(ctx, opts)
-	if err != nil {
-		d.tb.Fatalf("BeginTx failed: %s", err)
+	if rows.Next() {
+		tb.Fatal("more than one row returned")
 	}
-	return s, err
-}
-
-func (d *DBFatal) Close() error {
-	d.tb.Helper()
-	err := d.DB.Close()
-	if err != nil {
-		d.tb.Fatalf("Close failed: %s", err)
+	if err := rows.Err(); err != nil {
+		tb.Fatalf("rows.Err: %s", err)
 	}
-	return err
+	if err := rows.Close(); err != nil {
+		tb.Fatalf("Close return error: %s", err)
+	}
 }
