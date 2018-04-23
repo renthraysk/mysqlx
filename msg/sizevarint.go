d930e46d5d99bc2b2dee8ac7b124d38ba7378f80
renthraysk <renthraysk@gmail.com>
2018-04-23 18:55:38 +0100

    Comment typo

--- msg/sizevarint.go
+++ msg/sizevarint.go
@@ -2,7 +2,7 @@ package msg
 
 import "math/bits"
 
-// SizeVarint returns the number of bytes require to store a uint64 in base128/varint encoding
+// SizeVarint returns the number of bytes required to store a uint64 in base128/varint encoding
 func SizeVarint(x uint64) int {
 	// x|1 to prevent Len64() returning 0
 	return (bits.Len64(x|1) + 6) / 7
