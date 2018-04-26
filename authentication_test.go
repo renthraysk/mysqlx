39f224508da2d141f9b7a01365a28ba1c5a4d3b3
renthraysk <renthraysk@gmail.com>
2018-04-26 16:49:08 +0100

    Improve testing, with emptied cache, and populated

--- authentication_test.go
+++ authentication_test.go
@@ -1,6 +1,7 @@
 package mysqlx
 
 import (
+	"context"
 	"database/sql"
 	"testing"
 
@@ -8,15 +9,25 @@ import (
 	"github.com/renthraysk/mysqlx/authentication/sha256"
 )
 
+/*
+	CREATE USER usernative IDENTIFIED WITH 'mysql_native_password' BY 'passwordnative';
+	CREATE USER usersha256 IDENTIFIED WITH 'sha256_password' BY 'passwordsha256';
+	CREATE USER usersha2 IDENTIFIED WITH 'caching_sha2_password' BY 'passwordsha2';
+	FLUSH PRIVILEGES;
+*/
+
+func FlushAuthenticationCache(tb testing.TB) {
+	tb.Helper()
+
+	db := NewDB(tb)
+	defer db.Close()
+	_, err := db.ExecContext(context.Background(), "FLUSH PRIVILEGES")
+	if err != nil {
+		tb.Fatalf("Failed to flush privileges: %s", err)
+	}
+}
+
 func TestAuthentication(t *testing.T) {
-
-	/*
-		CREATE USER usernative IDENTIFIED WITH 'mysql_native_password' BY 'passwordnative';
-		CREATE USER usersha256 IDENTIFIED WITH 'sha256_password' BY 'passwordsha256';
-		CREATE USER usersha2 IDENTIFIED WITH 'caching_sha2_password' BY 'passwordsha2';
-		FLUSH PRIVILEGES;
-	*/
-
 	tests := map[string]struct {
 		network string
 		addr    string
@@ -62,19 +73,23 @@ func TestAuthentication(t *testing.T) {
 		},
 	}
 
-	//FlushAuthenticationCache(t)
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			connector, err := New(test.network, test.addr, test.options...)
-			if err != nil {
-				t.Fatalf("failed to create connector: %s", err)
-			}
-			db := sql.OpenDB(connector)
-			defer db.Close()
-			if err = db.Ping(); err != nil {
-				t.Fatalf("failed to ping: %s", err)
-			}
-		})
+	f := func(t *testing.T) {
+		for name, test := range tests {
+			t.Run(name, func(t *testing.T) {
+				connector, err := New(test.network, test.addr, test.options...)
+				if err != nil {
+					t.Fatalf("failed to create connector: %s", err)
+				}
+				db := sql.OpenDB(connector)
+				defer db.Close()
+				if err = db.Ping(); err != nil {
+					t.Fatalf("failed to ping: %s", err)
+				}
+			})
+		}
 	}
+
+	FlushAuthenticationCache(t)
+	t.Run("empty", f)
+	t.Run("cached", f)
 }
