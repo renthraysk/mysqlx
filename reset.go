d9fc9691d4102aba7b4533af477426163df5d178
renthraysk <renthraysk@gmail.com>
2018-04-27 14:04:54 +0100

    Wrong tense typo

--- reset.go
+++ reset.go
@@ -15,7 +15,7 @@ func NoSessionResetter(ctx context.Context, c *conn) error {
 	return nil
 }
 
-// HardSessionResetter a full connection reset. Transactions closed, prepare statements deleted, temporary tables dropped and session variables reset to global defaults
+// HardSessionResetter a full connection reset. Transactions closed, prepared statements deleted, temporary tables dropped and session variables reset to global defaults
 func HardSessionResetter(ctx context.Context, c *conn) error {
 	if err := c.send(ctx, msg.SessionReset(c.buf[:0])); err != nil {
 		return driver.ErrBadConn
