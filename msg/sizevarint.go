d4b6d9ffff8665614b0e9a3d5c324a01a8c9cdd3
renthraysk <renthraysk@gmail.com>
2019-01-22 17:43:45 +0000

    Add SizeUvarint variants

--- msg/sizevarint.go
+++ msg/sizevarint.go
@@ -7,3 +7,13 @@ func SizeUvarint64(x uint64) int {
 	// x|1 to prevent Len64() returning 0
 	return (bits.Len64(x|1) + 6) / 7
 }
+
+// SizeUvarint32 returns the number of bytes required to store a uint32 in base128/varint encoding
+func SizeUvarint32(x uint32) int {
+	// x|1 to prevent Len32() returning 0
+	return (bits.Len32(x|1) + 6) / 7
+}
+
+func SizeUvarint(x uint) int {
+	return (bits.Len(x|1) + 6) / 7
+}
