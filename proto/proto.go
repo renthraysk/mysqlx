c7275dda2438b7e2b3bf7e31a40b48cf49bd462b
renthraysk <renthraysk@gmail.com>
2020-04-27 00:13:51 +0100

    Comment

--- proto/proto.go
+++ proto/proto.go
@@ -20,6 +20,7 @@ const (
 	MaxVarintLen64 = 10
 )
 
+// EncodeBool return 1 when b is true, 0 otherwise
 func EncodeBool(b bool) byte {
 	if b {
 		return 1
@@ -53,7 +54,6 @@ func PutUvarint(b []byte, x uint64) int {
 func AppendWireString(p []byte, tag uint8, value string) []byte {
 	x := uint(len(value))
 	n := SizeVarint(x)
-
 	if bits.UintSize == 32 {
 		p = append(p, tag<<3|WireBytes, byte(x)|0x80, byte(x>>7)|0x80, byte(x>>14)|0x80, byte(x>>21)|0x80, byte(x>>28))
 		n += len(p) - MaxVarintLen32
