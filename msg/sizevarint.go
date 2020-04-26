bb731f0b652274782ba69d2a5f1d74cbbd658cad
renthraysk <renthraysk@gmail.com>
2020-04-26 17:55:20 +0100

    SizeUvarint optimization

--- msg/sizevarint.go
+++ msg/sizevarint.go
@@ -4,16 +4,14 @@ import "math/bits"
 
 // SizeUvarint64 returns the number of bytes required to store a uint64 in base128/varint encoding
 func SizeUvarint64(x uint64) int {
-	// x|1 to prevent Len64() returning 0
-	return (bits.Len64(x|1) + 6) / 7
+	return int(9*uint32(bits.Len64(x))+64) / 64
 }
 
 // SizeUvarint32 returns the number of bytes required to store a uint32 in base128/varint encoding
 func SizeUvarint32(x uint32) int {
-	// x|1 to prevent Len32() returning 0
-	return (bits.Len32(x|1) + 6) / 7
+	return int(9*uint32(bits.Len32(x))+64) / 64
 }
 
 func SizeUvarint(x uint) int {
-	return (bits.Len(x|1) + 6) / 7
+	return int(9*uint32(bits.Len(x))+64) / 64
 }
